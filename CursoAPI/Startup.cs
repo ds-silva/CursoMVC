827817fe7cbb62b7b9d09464b493d9e3c8b64299
David Barbosa da Silva <david.silva@minds.local>
2021-10-30 21:05:47 -0300

    Injetando dependências do swagger.

--- CursoAPI/Startup.cs
+++ CursoAPI/Startup.cs
@@ -1,13 +1,9 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
 
 namespace CursoAPI
 {
@@ -24,6 +20,7 @@ namespace CursoAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddSwaggerGen(c => { c.SwaggerDoc(name: "v1", new OpenApiInfo { Title = "CursoAPI", Version = "v1" }); });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
