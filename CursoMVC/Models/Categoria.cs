83c95faedc89e25485f44758710bf17f50c9d005
David Barbosa da Silva <david.silva@minds.local>
2021-10-31 14:49:31 -0300

    Adicionando scaffolded item ProdutosController.

--- CursoMVC/Models/Categoria.cs
+++ CursoMVC/Models/Categoria.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace CursoMVC.Models
 {
@@ -8,6 +7,5 @@ namespace CursoMVC.Models
         public int Id { get; set; }
         [Display(Name = "Descrição")]
         public string Descricao { get; set; }
-        public List<Produto> Produtos { get; set; }
     }
 }
